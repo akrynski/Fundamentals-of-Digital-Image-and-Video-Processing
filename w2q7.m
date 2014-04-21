ac4bec03165421c22feb95cf1b4d1a817b12fe61
akrynski <akrynski@users.noreply.github.com>
2014-04-21 21:24:45 +0200

    Update w2q7.m

--- w2q7.m
+++ w2q7.m
@@ -1,6 +1,7 @@
+%W2Q7 my answer to question 7 of week's 2 homework at Coursera's Introduction to Image and Video Processing
 %img64=double(imread('digital-images-week2_quizzes-lena.gif'))/255.0;%scaling
-img64 = im2double(imread('test.png'),'indexed')/255;%scaling
-%img64= im2double(imread('digital-images-week2_quizzes-lena.gif'));
+%img64 = im2double(imread('test.png'),'indexed')/255;%scaling
+img64= im2double(imread('digital-images-week2_quizzes-lena.gif'));
 
 figure('Units', 'pixels');
 figure(1);
@@ -15,8 +16,16 @@ filtered1 = imfilter(img64,Lo3x3,'replicate');
 subplot(2,3,2);
 imagesc(filtered1);colormap(gray);axis image;
 title('original filtered by lopass 3x3 filter');
-%MSE = calcMSE(img64,filtered1);
+tic;
+MSE = calcMSE(img64,filtered1);
+toc
+tic;
 MSE=sum( (img64(:) - filtered1(:)).^2 ) / numel(img64);
+toc;
+ tic;
+% MSE=sum((sum(img64-filtered1)).^2)/numel(img64);
+MSE=sum(sum((img64-filtered1).^2))/numel(img64);
+ toc
 fprintf('MSE = %f\n', MSE);
 PSNR = calcPSNR(MSE);
 fprintf('PSNR = %.2f\n', PSNR);
@@ -30,6 +39,19 @@ MSE = calcMSE(img64,filtered2);
 fprintf('MSE = %f\n', MSE);
 PSNR = calcPSNR(MSE);
 fprintf('PSNR = %.2f\n', PSNR);
+%------------------------------------------------------------------
+%------------------------------------------------------------------
+[ssimval, ssimmap] = ssim(img64,filtered1);
+fprintf('SSIM = %.2f\n', ssimval);
+subplot(2,3,5);
+imagesc(ssimmap),axis image;
+xlabel('ssim is better, human acceptable coefficient of differencebetween original and filtered image');
+title(sprintf('ssim Index Map\n - Mean ssim Value is %0.4f',ssimval));
+[ssimval, ssimmap] = ssim(img64,filtered2);
+fprintf('SSIM = %.2f\n', ssimval);
+subplot(2,3,6);
+imagesc(ssimmap),axis image;
+title(sprintf('ssim Index Map\n - Mean ssim Value is %0.4f',ssimval));
 %---------------------------------------------------------
 % t2w=wiener2(filtered2,[7,7]);
 % subplot(2,3,5);
